ace7291357bbdb6f1d4555745166491f782b8176
Vivek <vivekkamble762033@gmail.com>
2026-07-11 20:57:29 +0530

    added new things

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
+
 import streamlit as st
 import ollama
 
-st.set_page_config(
+st.set_page_config (
     page_title='Local AI Email Assistant',
     page_icon="✉️",
     layout="centered"
@@ -9,13 +10,16 @@ st.set_page_config(
 
 st.title("✉️ Local AI Email Responder")
 
+
 tone = st.selectbox(
     "Select Email Tone",
-    ["Professional", "Friendly", "Apologetic", "Direct"]
+    ["Professional", "Friendly",
+     "Apologetic", "Direct"]
+    
 )
 
 user_input = st.text_area(
-    "Enter your rough notes here:",
+    "Enter your rough areas here:",
     height=150
 )
 
@@ -26,24 +30,26 @@ if st.button("Generate Professional Email"):
         with st.spinner("Drafting your Email..."):
             system_prompt = f"""
             You are an expert corporate communicator.
-            Rewrite the notes into a well-structured email.
+            Rewrite the notes into well-structured email.
             Tone: {tone}.
             No explanations, just the email.
+            
             """
-            try:                                          # ✅ inside with block
-                response = ollama.chat(                  # ✅ fixed typo resposne → response
-                    model="llama3",
-                    messages=[
-                        {
-                            "role": "system",
-                            "content": system_prompt
-                        },
-                        {
-                            "role": "user",
-                            "content": user_input
-                        }
-                    ]
-                )
-                st.info(response["message"]["content"])  # ✅ fixed typo
-            except Exception as e:
-                st.error(f"Error: {e}. Is Ollama running?")  # ✅ fixed "Ia" → "Is"
+try:
+    resposne = ollama.chat(
+    model = "llama3",
+    messages= [
+            {
+                "role": "system",
+                "content": system_prompt},
+            { "role" : "user",
+             "content": user_input
+                
+            }    
+            
+    ]
+    )
+    
+    st.info (resposne["message"]["content"])
+except Exception as e:
+    st.error(f"Error: {e}. Ia Ollama running?")
